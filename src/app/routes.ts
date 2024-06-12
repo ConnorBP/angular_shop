36483d75a73a705a6ebea9bd99906b1f1c66ca1a
ConnorBP <connor.postma@gmail.com>
2024-06-12 11:24:17 -0700

    created login and cart page placeholders

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,6 +2,8 @@ import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
 import { DetailsComponent } from "./details/details.component";
+import { LoginComponent } from "./login/login.component";
+import { CartComponent } from "./cart/cart.component";
 
 const routeConfig: Routes = [
     {
@@ -15,10 +17,20 @@ const routeConfig: Routes = [
         title: 'About Us'
     },
     {
+        path: 'login',
+        component: LoginComponent,
+        title: 'User Login'
+    },
+    {
       path: 'details/:id',
       component: DetailsComponent,
       title: 'Product details'
-    }
+    },
+    {
+        path: 'cart',
+        component: CartComponent,
+        title: 'Shopping Cart'
+    },
   ];
   
   export default routeConfig;
