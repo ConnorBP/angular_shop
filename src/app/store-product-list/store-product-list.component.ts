abbe8da496e58edcf083f92ea7d8b0755107b77b
ConnorBP <connor.postma@gmail.com>
2024-06-13 12:06:07 -0700

    Added spinner to main products listing page

--- src/app/store-product-list/store-product-list.component.ts
+++ src/app/store-product-list/store-product-list.component.ts
@@ -4,7 +4,8 @@ import { NgFor, NgIf } from '@angular/common';
 
 // components
 import { StoreProductComponent  } from '../store-product/store-product.component';
-import { StoreProduct } from '../store-product';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 
 // services
 import { ProductsDatabaseService } from '../products-database.service';
@@ -15,17 +16,29 @@ import { ProductsDatabaseService } from '../products-database.service';
   standalone: true,
   imports: [
     StoreProductComponent,
+    MatProgressSpinnerModule,
     NgFor,
     NgIf,
   ],
   template: `
     <h2>Products</h2>
-    <div id="products" class="grid-container-4 grid-spaced">
-      <store-product
-        *ngFor="let productInformation of productsDatabaseService.storeProducts"
-        [productInformation]="productInformation">
-      </store-product>
+    <div *ngIf="productsDatabaseService.productsLoaded; else spinner">
+      <div id="products" class="grid-container-4 grid-spaced">
+        <store-product
+          *ngFor="let productInformation of productsDatabaseService.storeProducts"
+          [productInformation]="productInformation">
+        </store-product>
+      </div>
     </div>
+    <ng-template #spinner>
+        <div>
+            <mat-progress-spinner
+                class="center"
+                color="red"
+                mode="indeterminate">
+            </mat-progress-spinner>
+        </div>
+    </ng-template>
   `,
   styleUrl: './store-product-list.component.css'
 })
