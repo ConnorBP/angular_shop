fd0da6ff7e3dbf2c2bb3e2ca775f0ca4b0084314
ConnorBP <connor.postma@gmail.com>
2024-06-14 15:35:10 -0700

    filled out checkout page with some starter form components and layout

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css'
 })
