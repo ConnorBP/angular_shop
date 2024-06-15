440846306dda6395826505814e81bb039626d3f8
ConnorBP <connor.postma@gmail.com>
2024-06-14 17:22:17 -0700

    Added basic tax calculation and form elements to checkout page

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatStepperModule } from '@angular/material/stepper';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { ShoppingCartService } from '../shopping-cart.service';
+import Utils from '../utils';
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
   imports: [
     MatCardModule,
+    MatStepperModule,
     FormsModule,
+    ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
@@ -21,5 +26,22 @@ import { MatInputModule } from '@angular/material/input';
   styleUrl: './checkout.component.css'
 })
 export class CheckoutComponent {
+  contactInfoFormGroup = this._formBuilder.group({
+    contactName: ['', Validators.required],
+    contactMethod: ['', Validators.required],
+  });
 
+  shippingMethodFormGroup = this._formBuilder.group({
+    shipOrPickup: ['', Validators.required],
+  })
+
+  get totalAfterTaxesFormatted() {
+    return Utils.formatPrice(this.cartService.cartPrice + Utils.calculateTaxes(this.cartService.cartPrice));
+  }
+
+  get totalTaxesFormatted() {
+    return Utils.formatPrice(Utils.calculateTaxes(this.cartService.cartPrice));
+  }
+
+  constructor(private _formBuilder: FormBuilder, public cartService: ShoppingCartService) {}
 }
--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,8 +1,16 @@
 export default class Utils {
+
+    // returns a locale formatted currency display string for a number
     static formatPrice(price: number): string {
         return `${Number(price).toLocaleString(undefined, {
           style: "currency",
           currency: "CAD" // todo: put store currency in a config file/state. For demo this is fine.
         })}`;
-      }
+    }
+
+    // returns the taxes on a provided sales value.
+    // For demo purposes we use a hard coded tax of 13%
+    static calculateTaxes(price: number): number {
+      return price * 0.13;
+    }
 }
